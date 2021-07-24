b91dd08de96605f743322c65f55c9e612364a5cb
Aayush Jain <jaayush014@gmail.com>
2021-07-24 19:43:18 +0530

    shemas and resolvers

--- app.js
+++ app.js
@@ -1,12 +1,40 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { graphqlHTTP } = require('express-graphql');
+const { buildSchema } = require('graphql');
 
 const app = express();
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
-app.get('/', (req, res)=>{
-	res.send('Hello World')
-})
+app.use(
+	'/graphql',
+	graphqlHTTP({
+		schema: buildSchema(`
+		type RootQuery {
+			events: [String!]!
+		}
+
+		type RootMutation {
+			createEvent(name: String): String
+		}
+
+		schema {
+			query: RootQuery 
+			mutation: RootMutation
+		}
+	`),
+		rootValue: {
+			events: () => {
+				return ['Naruto', 'Real Madrid', 'Coding'];
+			},
+			createEvent: (args) => {
+				const eventName = args.name;
+				return eventName;
+			},
+		},
+		graphiql: true,
+	})
+);
 
 app.listen(5000)
