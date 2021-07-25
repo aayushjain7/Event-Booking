6ad7587ebae8ff2391b157aad5533e2b4947cf02
Aayush Jain <jaayush014@gmail.com>
2021-07-25 18:31:33 +0530

    connect with mongodb

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { graphqlHTTP } = require('express-graphql');
 const { buildSchema } = require('graphql');
+const mongoose = require('mongoose');
+require('dotenv').config()
+
+const Event = require('./models/event');
 
 const app = express();
-
-const events = [];
 
 app.use(bodyParser.json());
 
@@ -43,22 +45,45 @@ app.use(
 	`),
 		rootValue: {
 			events: () => {
-				return events;
+				return Event.find()
+          .then(events => {
+            return events.map(event => {
+              return { ...event._doc, _id: event.id };
+            });
+          })
+          .catch(err => {
+            throw err;
+          });
 			},
 			createEvent: (args) => {
-				const event = {
-          _id: Math.random().toString(),
+				const event = new Event({
           title: args.eventInput.title,
           description: args.eventInput.description,
           price: +args.eventInput.price,
-          date: args.eventInput.date
-        };
-        events.push(event);
-        return event;
+          date: new Date(args.eventInput.date)
+        });
+        return event
+          .save()
+          .then(result => {
+            console.log(result);
+            return { ...result._doc, _id: result._doc._id.toString() };
+          })
+          .catch(err => {
+            console.log(err);
+            throw err;
+          });
 			},
 		},
 		graphiql: true,
 	})
 );
 
-app.listen(5000)
+mongoose
+  .connect(process.env.MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true})
+  .then(() => {
+    app.listen(5000);
+		console.log("MONGODB and Server Connected!")
+  })
+  .catch(err => {
+    console.log(err);
+  });
